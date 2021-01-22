3803a0265137ecf37704ece3ed45a8ca66729365
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-01-22 16:21:57 +0000

    add basic message parsing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello bot")
+require('dotenv').config();
+
+const eightball = require('8ball')
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.endsWith("?")) {
+        msg.reply(eightball());
+    }
+});
